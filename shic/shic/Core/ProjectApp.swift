49bfd0696c37f0aa624953d5599298580a818f28
jarw1th <parastaev31@gmail.com>
2024-10-04 11:48:33 +0300

    init: Xcode

---
+++ shic/shic/Core/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  shicApp.swift
+//  shic
+//
+//  Created by Руслан Парастаев on 04.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct shicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            
+        }
+    }
+}
