14f58a16c58c87a4a45df4a528492906776f0d14
jarw1th <parastaev31@gmail.com>
2024-10-06 17:13:57 +0300

    fix: Route manager renamed

---
+++ shic/shic/ViewModels/Route Managers/SmartFormRouteManager.swift
@@ -0,0 +1,69 @@
+//
+//  SmartFormRouteManager.swift
+//  shic
+//
+//  Created by Руслан Парастаев on 05.10.2024.
+//
+
+import SwiftUI
+
+final class SmartFormRouteManager {
+    
+    static let shared = SmartFormRouteManager()
+    
+    private var viewModel: ViewModel? = nil
+    
+    private(set) var formIndex: Int
+    private var screenIndex: Int
+    private(set) var formScreens: [any View]
+    private var allScreens: [any View]
+    
+    init() {
+        self.formIndex = 0
+        self.screenIndex = 0
+        self.formScreens = [SmartFormGenderScreen(), SmartFormWearStyleScreen(), SmartFormParamsScreen(), SmartFormBodyTypeScreen(), SmartFormSizesScreen(),
+                            SmartFormShirtScreen(), SmartFormWearTypeScreen()]
+        self.allScreens = [StartSmartFormScreen(), SmartFormGenderScreen(), SmartFormWearStyleScreen(), SmartFormParamsScreen(), SmartFormBodyTypeScreen(), SmartFormSizesScreen(),
+                           SmartFormShirtScreen(), SmartFormWearTypeScreen()]
+    }
+    
+    func initial(_ viewModel: ViewModel) {
+        self.viewModel = viewModel
+        self.formIndex = 0
+        self.screenIndex = 0
+        self.formScreens = [SmartFormGenderScreen(), SmartFormWearStyleScreen(), SmartFormParamsScreen(), SmartFormBodyTypeScreen(), SmartFormSizesScreen(),
+                            SmartFormShirtScreen(), SmartFormWearTypeScreen()]
+        self.allScreens = [StartSmartFormScreen(), SmartFormGenderScreen(), SmartFormWearStyleScreen(), SmartFormParamsScreen(), SmartFormBodyTypeScreen(), SmartFormSizesScreen(),
+                           SmartFormShirtScreen(), SmartFormWearTypeScreen()]
+    }
+    
+    func pop(_ decrementIndex: Bool = true) {
+        if decrementIndex {
+            formIndex -= 1
+        }
+        screenIndex -= 1
+    }
+    
+    func getScreen() -> any View {
+        if allScreens.count > screenIndex,
+           let viewModel {
+            let screen = allScreens[screenIndex].navigationBarHidden(true).environmentObject(viewModel)
+            return screen
+        } else {
+            return getLaterScreen()
+        }
+    }
+    
+    func push(_ incrementIndex: Bool = true) {
+        if incrementIndex {
+            formIndex += 1
+        }
+        screenIndex += 1
+    }
+    
+    func getLaterScreen() -> any View {
+        let screen = LoadingScreen()
+        return screen
+    }
+    
+}
